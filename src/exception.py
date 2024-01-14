28601b5d2ce9fe3243350195294293823798a483
vedchaudhari <vedc2853@gmail.com>
2024-01-14 23:49:50 +0530

    EDA

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from logger import logging
 
 def error_message_details(error,error_detail:sys):
     _,_,exe_tb=error_detail.exc_info()
@@ -15,12 +15,6 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-    
-logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
-)
     
 if __name__=="__main__":
     try:
